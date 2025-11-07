440decdcb4f754ed1acfa99cf760e5f979ef1045
MrBig83 <mjohansson176@gmail.com>
2025-11-07 17:08:23 +0100

    Ett test för att se om build funkar på GitHub

--- Program.cs
+++ Program.cs
@@ -9,7 +9,7 @@ namespace SnackToSixPack
         {
             //MenuHandler.ShowUserMenu();
             //AuthForms.ShowLogInForm();
-            Authentication.TwoFactorAuth();
+            Authentication.TwoFactorAuth(); 
             
             //AuthForms.ShowLogInForm();
 
