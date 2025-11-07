d0d52ae38258d24fc9e3b9c3e65eb934002e4d39
MrBig83 <mjohansson176@gmail.com>
2025-11-07 19:07:41 +0100

    Testar lite till

--- Program.cs
+++ Program.cs
@@ -11,7 +11,7 @@ namespace SnackToSixPack
             //AuthForms.ShowLogInForm();
             Authentication.TwoFactorAuth(); 
             
-            //AuthForms.ShowLogInForm();
+            //AuthForms.ShowLogInForm(); 
 
             //// ======= Detta är bara för att provköra hämtning av JSON och User-klassen =======
             //List<User> users = JsonHelper.LoadUsers();
