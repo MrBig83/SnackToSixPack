efe796be5654bc8095d83666ea4dc870618deb34
MrBig83 <mjohansson176@gmail.com>
2025-11-10 13:52:02 +0100

    Added User-specific folders and populated one with dummy data. Also created Classes for workouts.

---
+++ Classes/Workout.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class Workout
+{
+    public string WorkoutId { get; set; }
+    public DateTime Date { get; set; }
+    public string Title { get; set; }
+    public List<Exercise> Exercises { get; set; } = new List<Exercise>();
+}
