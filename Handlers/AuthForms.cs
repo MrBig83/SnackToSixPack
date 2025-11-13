eacb76484fa6a2f1e29360b15d66e88ad6477484
emeljana <emeliecaroline99@gmail.com>
2025-11-13 10:06:43 +0100

    fix so the new registered user is the current user

--- Handlers/AuthForms.cs
+++ Handlers/AuthForms.cs
@@ -79,7 +79,6 @@ namespace SnackToSixPack.Classes
 
                 AnsiConsole.WriteLine();
                 AnsiConsole.Markup("[bold]Welcome[/]" + ",[green] " + usernameInput + "[/]!");
-                MenuHandler.ShowUserMenu();
                 running = false;
             }
         }
