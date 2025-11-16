30c1d946989af3aae1fe07499ba5c5184c0045d6
MrBig83 <mjohansson176@gmail.com>
2025-11-16 16:43:13 +0100

    Added Nullable values to Exercises-class.

--- Classes/Exercise.cs
+++ Classes/Exercise.cs
@@ -2,8 +2,8 @@
 {
     public string ExerciseId { get; set; }
     public string Name { get; set; }
-    public int Sets { get; set; }
-    public int Reps { get; set; }
+    public int? Sets { get; set; }
+    public int? Reps { get; set; }
     public double? Weight { get; set; }   // Använd nullable för att tillåta null i JSON
     public int? RestTime { get; set; }    // Sekunder, nullable
     public string Notes { get; set; }
