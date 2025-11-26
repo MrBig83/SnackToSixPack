3e0661b992b044d4f790156185b1925b8cbdd700
emeljana <emeliecaroline99@gmail.com>
2025-11-26 11:08:11 +0100

    Add centralized try/catch to JSONFileHandler to avoid muliple error handling

--- Handlers/JSONFileHandler.cs
+++ Handlers/JSONFileHandler.cs
@@ -1,25 +1,40 @@
 using System.Text.Json;
 
+namespace SnackToSixPack.Classes;
 public static class JSONFileHanldler<T>
 {
     public static T Load<T>(string filePath)
     {
-        if (!File.Exists(filePath))
-        throw new FileNotFoundException($"File not found: {filePath}");
-
-        string json = File.ReadAllText(filePath);
-        return JsonSerializer.Deserialize<T>(json);
+        try
+        {
+            if (!File.Exists(filePath))
+                throw new FileNotFoundException($"File not found: {filePath}");
+            
+            string json = File.ReadAllText(filePath);
+            return JsonSerializer.Deserialize<T>(json);
+        }
+        catch (Exception ex)
+        {
+            Logger.LogError($"Error loading file at {filePath}: {ex.Message}");
+            return default;
+        }
     }
 
     public static void Save<T>(string filePath, T data)
     {
-        var options = new JsonSerializerOptions
+        try
         {
-            WriteIndented = true
-        };
-
-        string json = JsonSerializer.Serialize(data, options);
-        File.WriteAllText(filePath, json);
+            string json = JsonSerializer.Serialize(data, new JsonSerializerOptions()
+            {
+                WriteIndented = true
+            });
+            File.WriteAllText(filePath, json);
+        }
+        catch (Exception ex)
+        {
+            Logger.LogError($"Error saving file at {filePath}: {ex.Message}");
+        }
+        
     }
 
 }
